9f7fd1764e7418be765903184880abf90758e198
cgomez <cgomez@truora.com>
2024-02-24 17:01:01 -0500

    python file v2

--- example.py
+++ example.py
@@ -1,3 +1,10 @@
 #my first action
 
 print("Hello World")
+#create a file 
+
+
+with open("file"".txt", "w") as file:
+    for i in range(10):
+        #create a file and write to it
+        file.write("This is line %d\n" % (i+1))
