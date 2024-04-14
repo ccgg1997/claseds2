10a827b7f8720d8333dd1afa5c237257ecb2ac35
Cristian Gómez <89625031+ccgg1997@users.noreply.github.com>
2024-04-14 18:44:41 +0000

    test22

--- example.py
+++ example.py
@@ -7,6 +7,6 @@ print("Hello World")
 with open("file"".txt", "w") as file:
     for i in range(10):
         #create a file and write to it
-        print("hello worldñññññx89")
-        print("hello worlñññññññx89")
+        print("hello worldñññññx8910")
+        print("hello worlñññññññx8910")
         file.write("This is line %d\n" % (i+1))
